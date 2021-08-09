ebc8e3ec70cb1388637f491a9ed212ae9fc58c99
santtose <santtose@outlook.com>
2021-08-09 11:13:25 -0300

    comportamentos basicos api catalogo

---
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Controllers
+{
+    public class CatalogoController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CatalogoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        //[AllowAnonymous]
+        [HttpGet("catalogo/produtos")]
+        public async Task<IEnumerable<Produto>> Index()
+        {
+            return await _produtoRepository.ObterTodos();
+        }
+
+        //[ClaimsAuthorize("Catalogo", "Ler")]
+        [HttpGet("catalogo/produtos/{id}")]
+        public async Task<Produto> ProdutoDetalhe(Guid id)
+        {
+            return await _produtoRepository.ObterPorId(id);
+        }
+    }
+}
