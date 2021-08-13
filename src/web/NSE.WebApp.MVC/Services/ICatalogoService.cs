3acc4f28889a0ab894849c51b62cc991d18d5e43
santtose <santtose@outlook.com>
2021-08-13 10:13:40 -0300

    refit, retry com polly e circuit breaker

--- src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
+++ src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using NSE.WebApp.MVC.Models;
+using Refit;
 
 namespace NSE.WebApp.MVC.Services
 {
@@ -11,12 +12,12 @@ namespace NSE.WebApp.MVC.Services
         Task<ProdutoViewModel> ObterPorId(Guid id);
     }
 
-    //public interface ICatalogoServiceRefit
-    //{
-    //    [Get("/catalogo/produtos/")]
-    //    Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+    public interface ICatalogoServiceRefit
+    {
+        [Get("/catalogo/produtos/")]
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
 
-    //    [Get("/catalogo/produtos/{id}")]
-    //    Task<ProdutoViewModel> ObterPorId(Guid id);
-    //}
+        [Get("/catalogo/produtos/{id}")]
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+    }
 }
