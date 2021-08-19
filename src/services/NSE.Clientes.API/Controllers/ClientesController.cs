45c1c95bd2863149db09a1f3001b101a459eb9ad
santtose <santtose@outlook.com>
2021-08-19 14:19:42 -0300

    pastas cliente api

---
+++ src/services/NSE.Clientes.API/Controllers/ClientesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Controllers
+{
+    public class ClientesController
+    {
+        //private readonly IMediatorHandler _mediatorHandler;
+
+        //public ClientesController(IMediatorHandler mediatorHandler)
+        //{
+        //    _mediatorHandler = mediatorHandler;
+        //}
+
+        //[HttpGet("clientes")]
+        //public async Task<IActionResult> Index()
+        //{
+        //    var resultado = await _mediatorHandler.EnviarComando(
+        //        new RegistrarClienteCommand(Guid.NewGuid(), "Eduardo", "edu@edu.com", "30314299076"));
+
+        //    return CustomResponse(resultado);
+        //}
+    }
+}
